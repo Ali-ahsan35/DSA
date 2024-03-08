c10bec2bbf1386060a3aea4614eb430548edf349
Ali-ahsan35 <syedahsan748@gmail.com>
2024-03-09 01:41:20 +0600

    function to calculate prime number

---
+++ primeNumber.js
@@ -0,0 +1,16 @@
+function primeNumber(n) {
+    if (n < 2) {
+        return false;
+    }
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const result = primeNumber(4);
+console.log(result);
+
+// Big-O ---> O(n)
