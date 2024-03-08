be0a8b4002ccddc6687b9bcc9e10025ac8368fda
Ali-ahsan35 <syedahsan748@gmail.com>
2024-03-09 01:15:52 +0600

    function to calculate factorial of a number

---
+++ factorial.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+const result = factorial(5);
+console.log(result);
+
+// Big-O ---> O(n)
