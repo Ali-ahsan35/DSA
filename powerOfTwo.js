b7f65ffeac781ccef7642b94c705cc0350f4aeaf
Ali-ahsan35 <syedahsan748@gmail.com>
2024-03-09 09:00:45 +0600

    another solution of power of two with constant time complexity

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -15,3 +15,17 @@ const result = isPowerOfTwo(5);
 console.log(result);
 
 // Big-O ---> O(logn)
+
+// Another solution with constant time complexity
+
+function isPowerOfTwoBitWise(n) {
+    if (n<1) {
+        return false;
+    }
+
+    return (n & (n-1)) === 0;
+}
+const result2 = isPowerOfTwoBitWise(8);
+console.log(result2);
+
+// Big-O ---> O(1)
